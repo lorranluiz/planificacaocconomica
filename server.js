ca8acfcc236cb67e1d7c6964584519e2cf107bd1
lorranluiz <lorranluiz.neki@gmail.com>
2025-02-02 09:54:26 -0300

    Refactor SSL certificate paths for production environment

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ function isLocalEnvironment(req) {
 let productionSSL = null;
 try {
   productionSSL = {
-    key: fs.readFileSync(path.join(__dirname, 'privkey.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'fullchain.pem')),
+    key: fs.readFileSync(path.join(__dirname, 'cert.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'chain.pem')),
   };
 } catch (error) {
   console.log('Certificados de produção não encontrados, usando apenas certificados locais');
